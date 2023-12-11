6f8cae429aa8d40b67ba6c6eb1127b2349e564c3
Andrii Haranin <andrii.haranin@teladochealth.com>
2023-12-11 21:50:54 +0100

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,19 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  
+  devise_for :users
+
+  resources :blog_posts
+
+  # This ↓ replaced by this ↑ resources method.
+  #
+  # get "/blog_posts/new", to: "blog_posts#new", as: :new_blog_post
+  # get "/blog_posts/:id", to: "blog_posts#show", as: :blog_post
+  # patch "/blog_posts/:id", to: "blog_posts#update"
+  # delete "/blog_posts/:id", to: "blog_posts#delete"
+  # get "/blog_posts/:id/edit", to: "blog_posts#edit", as: :edit_blog_post
+  # post "/blog_posts", to: "blog_posts#create", as: :blog_posts
+
+  # Defines the root path route ("/")
+  root "blog_posts#index"
+end
