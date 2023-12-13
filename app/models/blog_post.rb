96568d63a1ae099eecfb1efc9d67dc190f958fef
Andrii Haranin <andrii.haranin@teladochealth.com>
2023-12-13 18:53:30 +0100

    Adds ActionText and Amazon S3 bucket as image storage

--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -1,6 +1,8 @@
 class BlogPost < ApplicationRecord
+  has_rich_text :content
+
   validates :title, presence: true
-  validates :body, presence: true
+  validates :content, presence: true
 
   scope :sorted, -> { order(arel_table[:published_at].desc.nulls_first).order(published_at: :desc) }
   scope :draft, -> { where(published_at: nil) }
